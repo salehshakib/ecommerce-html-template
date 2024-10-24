cdcceac0002bf219293234fc6fb5ad923cfbcca2
salehshakib <salehshakib.ss@gmail.com>
2024-10-24 13:25:55 +0600

    password update

--- asset/js/toggle-password.js
+++ asset/js/toggle-password.js
@@ -18,3 +18,51 @@ togglePasswordButton.addEventListener("click", function () {
     toggleIcon.alt = "Show Password";
   }
 });
+
+const togglePasswordButtonNew = document.getElementById("toggle-new-password");
+const passwordInputNew = document.getElementById("newPassword");
+const toggleIconNew = document.getElementById("toggle-new-icon");
+
+togglePasswordButtonNew.addEventListener("click", function () {
+  // Toggle the type attribute
+  const isPasswordHidden = passwordInputNew.getAttribute("type") === "password";
+  passwordInputNew.setAttribute("type", isPasswordHidden ? "text" : "password");
+
+  // Toggle the eye icon based on the visibility
+  if (isPasswordHidden) {
+    toggleIconNew.src =
+      "https://cdn.jsdelivr.net/gh/vitasoftsolutions/assets-admin@latest/blade-admin-template/asset/icon/eye-slash-svgrepo-com.svg"; // Path to your 'hide' icon
+    toggleIconNew.alt = "Hide Password";
+  } else {
+    toggleIconNew.src =
+      "https://cdn.jsdelivr.net/gh/vitasoftsolutions/assets-admin@latest/blade-admin-template/asset/icon/eye-svgrepo-com.svg"; // Path to your 'show' icon
+    toggleIconNew.alt = "Show Password";
+  }
+});
+
+const togglePasswordButtonConfirm = document.getElementById(
+  "toggle-confirm-password"
+);
+const passwordInputConfirm = document.getElementById("confirmPassword");
+const toggleIconConfirm = document.getElementById("toggle-confirm-icon");
+
+togglePasswordButtonConfirm.addEventListener("click", function () {
+  // Toggle the type attribute
+  const isPasswordHidden =
+    passwordInputConfirm.getAttribute("type") === "password";
+  passwordInputConfirm.setAttribute(
+    "type",
+    isPasswordHidden ? "text" : "password"
+  );
+
+  // Toggle the eye icon based on the visibility
+  if (isPasswordHidden) {
+    toggleIconConfirm.src =
+      "https://cdn.jsdelivr.net/gh/vitasoftsolutions/assets-admin@latest/blade-admin-template/asset/icon/eye-slash-svgrepo-com.svg"; // Path to your 'hide' icon
+    toggleIconConfirm.alt = "Hide Password";
+  } else {
+    toggleIconConfirm.src =
+      "https://cdn.jsdelivr.net/gh/vitasoftsolutions/assets-admin@latest/blade-admin-template/asset/icon/eye-svgrepo-com.svg"; // Path to your 'show' icon
+    toggleIconConfirm.alt = "Show Password";
+  }
+});
